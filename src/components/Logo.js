2657c9e9a1eae8151c05064a9957b7d1eb5c24b0
Anuj Verma <113152048+DevAnujV@users.noreply.github.com>
2023-09-04 15:50:18 -0400

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,3 +1,8 @@
 export default function Logo() {
-  return <h1>🌴 Far Away 💼</h1>;
-}
+  return (
+    <>
+      <h1>🌴 Far Away 💼</h1>
+      <p className="headPara">-by Anuj Verma</p>
+    </>
+  );
+      }
